c814aad30412d42c08a5fa6ccdf24509f910d865
Nick Pepin <31704762+Warpep19969@users.noreply.github.com>
2019-06-17 00:32:23 -0400

    Update camera.py

--- main/common/camera.py
+++ main/common/camera.py
@@ -1,12 +1,6 @@
 import os
-import subprocess, shlex
 import time
 import win32com.client
-from win32com.client import Dispatch
-import signal
-import winreg
-import math
-import pywintypes
 
 # NEEDS TESTING
 
@@ -14,40 +8,57 @@ class Camera():
     def __init__(self, output_directory):
         # output_directory starts from user path
         self.output_directory = output_directory
-        self.camera = win32com.client.Dispatch("MaxIm.CCDCamera")  # Sets the camera connection path to the CCDCamera
+        self.Camera = win32com.client.Dispatch("MaxIm.CCDCamera")  # Sets the camera connection path to the CCDCamera
+        self.Camera.DisableAutoShutdown == True  # All of these settings are just basic camera setup settings.
+        self.Camera.LockApp == True
+        self.Camera.CoolerOn == True
+        self.Camera.AutoDownload == True
 
         try:
-            self.camera.LinkEnabled = True
+            self.Camera.LinkEnabled == True
             print("Camera is connected")
 
         except:
             print("Camera is not Connected")
             return
 
-        self.camera.DisableAutoShutdown = True  # All of these settings are just basic camera setup settings.
-        self.camera.AutoDownload = True
-
     def expose(self, exposure_time, filter, type="light"):
         if type == "light":
             type = 1
         elif type == "dark":
             type = 0
-        else:
-            print("ERROR: Invalid exposure type.")
-            return
-        self.camera.SetFullFrame()
-        self.camera.Expose(exposure_time, type, filter)
-        time.sleep(exposure_time)
-        while self.camera.ImageReady==False:
+        self.Camera.SetFullFrame()
+        self.Camera.Expose(exposure_time, type, filter)
+        while Camera.ImageReady==False:
             time.sleep(1)
-            if self.camera.ImageReady:
-                self.camera.StartDownload
-                path = os.path.expanduser('~')
-                self.camera.SaveImage(os.path.join(path, 'Desktop', self.output_directory, "test_pictures.fit"))
-
+            if Camera.ImageReady:
+                Camera.StartDownload
+                path = os.path.expanduser(self.output_directory)
+                Camera.SaveImage(os.path.join(path, "test_pictures.fit"))
 
     def set_gain(self):
         pass
 
     def set_binning(self):
         pass
+
+    def log(self):
+        return self.camera.CameraState
+        a="Camera is Idle: Able to start exposures"
+        b="Camera is Waiting: Exposure has started"
+        c="Camera is Exposing: Exposure is currently in process"
+        d="Camera is Reading: Camera array is being read out"
+        e="Camera is Downloading: Downloading data to PC"
+        f="CAMERA ERROR!!!"
+        if self.camera.CameraState == 0:
+            return a
+        elif self.camera.CameraState == 1:
+            return b
+        elif self.camera.CameraState == 2:
+            return c
+        elif self.camera.CameraState == 3:
+            return d
+        elif self.camera.CameraState == 4:
+            return e
+        elif self.camera.CameraState == 5:
+            return f
