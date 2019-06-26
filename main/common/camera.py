6e87990f0a482a50eb7628b8aad67a5f18fc2e81
Nick Pepin <31704762+Warpep19969@users.noreply.github.com>
2019-06-26 01:55:58 -0400

    Update camera.py

--- main/common/camera.py
+++ main/common/camera.py
@@ -2,39 +2,43 @@ import os
 import time
 import win32com.client
 
+
 # NEEDS TESTING
 
 class Camera():
     def __init__(self, output_directory):
         # output_directory starts from user path
         self.output_directory = output_directory
-        self.Camera = win32com.client.Dispatch("MaxIm.CCDCamera")  # Sets the camera connection path to the CCDCamera
-        self.Camera.DisableAutoShutdown == True  # All of these settings are just basic camera setup settings.
-        self.Camera.LockApp == True
-        self.Camera.CoolerOn == True
-        self.Camera.AutoDownload == True
+        self.camera = win32com.client.Dispatch("MaxIm.CCDCamera")  # Sets the camera connection path to the CCDCamera
 
         try:
-            self.Camera.LinkEnabled == True
+            self.camera.LinkEnabled = True
             print("Camera is connected")
 
         except:
             print("Camera is not Connected")
             return
 
+        self.camera.DisableAutoShutdown = True  # All of these settings are just basic camera setup settings.
+        self.camera.AutoDownload = True
+
     def expose(self, exposure_time, filter, type="light"):
         if type == "light":
             type = 1
         elif type == "dark":
             type = 0
-        self.Camera.SetFullFrame()
-        self.Camera.Expose(exposure_time, type, filter)
-        while Camera.ImageReady==False:
+        else:
+            print("ERROR: Invalid exposure type.")
+            return
+        self.camera.SetFullFrame()
+        self.camera.Expose(exposure_time, type, filter)
+        time.sleep(exposure_time)
+        while self.camera.ImageReady==False:
             time.sleep(1)
-            if Camera.ImageReady:
-                Camera.StartDownload
-                path = os.path.expanduser(self.output_directory)
-                Camera.SaveImage(os.path.join(path, "test_pictures.fit"))
+            if self.camera.ImageReady:
+                self.camera.StartDownload
+                path = os.path.expanduser('~')
+                self.camera.SaveImage(os.path.join(path, 'Desktop', "test_pictures.fit"))
 
     def set_gain(self):
         pass
