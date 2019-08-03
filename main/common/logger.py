562fc361743db789924750a33710f2fb48a1d01d
Nick Pepin <31704762+Warpep19969@users.noreply.github.com>
2019-08-02 23:55:25 -0400

    Update logger.py

--- main/common/logger.py
+++ main/common/logger.py
@@ -9,13 +9,11 @@ from main.common.cAmera import *
 # ERROR: More serious problem
 # CRITICAL: Very Serious error
 
-class Logger(Camera):
-    # logging.basicConfig(filename="C:\\Users\\Nicholas Pepin\\Desktop\\CameraErrorLog.log", level=logging.DEBUG, filemode='w', format="%(asctime)s - %(levelname)s - %(message)s")
-    # logger = logging.getLogger()  #These two lines contain a scrunched version of below but without handlers
 
-    # Creates 'cAmera' logger
-    logger = logging.getLogger('cAmera')
-    logger.setLevel(logging.DEBUG)
+class main:
+    # logging.basicConfig(filename="C:\\Users\\Nicholas Pepin\\Desktop\\CameraErrorLog.log", level=logging.DEBUG,
+    # filemode='w', format="%(asctime)s - %(levelname)s - %(message)s") logger = logging.getLogger()  #These two
+    # lines contain a scrunched version of below but without handlers
     # creates filehandlers
     fh = logging.FileHandler('C:\\Users\\Nicholas Pepin\\Desktop\\CameraErrorLog.log')
     fh.setLevel(logging.DEBUG)
@@ -27,13 +25,15 @@ class Logger(Camera):
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     # adds handlers to the logger
+    logger = logging.getLogger()
     logger.addHandler(fh)
     logger.addHandler(ch)
+    logger.setLevel(logging.DEBUG)
 
-    logger.info('Camera process has started')
-    # Creating a link to Camera()
-    logger.info('Created an instance of Camera log')
-    logger.info('Collecting Camera logging information')
-    a = cAmera.Camera('camera_work')        #idk why 'a' from the 'def log()' function does not transfer over. Need help
-    a.log()                                 #supposed to run log() and have a returned
+    logger.info('Camera process has started : ' + __name__)
+    cAmera.Camera.log()  # need to fulfil self
+
+    # Theoretically, it works. but in 'cAmera.Camera.log()' it says that self needs to be fulfilled
+    # and I have no idea how to do that. If someone could help guide me to a paper or video that would
+    # explain it, that would be very helpful.
     
