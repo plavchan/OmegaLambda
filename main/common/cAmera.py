58463b9494f6354882e52d53227cb497bf3cbc3e
Nick Pepin <31704762+Warpep19969@users.noreply.github.com>
2019-08-02 23:54:34 -0400

    Update and rename camera.py to cAmera.py

--- main/common/cAmera.py
+++ main/common/cAmera.py
@@ -2,27 +2,25 @@ import os
 import time
 import win32com.client
 import logging
-      
+
 
 # NEEDS TESTING
 
+
 class Camera:
     def __init__(self, output_directory):
-        __all__ = ['__init__', 'Camera']                    #for some reason, opens up the class and __init__ file?
+        # __all__ = ['__init__', 'Camera']  # for some reason, opens up the class and __init__ file?
         # output_directory starts from user path
+        self.logger = logging.getLogger(__name__)
         self.output_directory = output_directory
         self.camera = win32com.client.Dispatch("MaxIm.CCDCamera")  # Sets the camera connection path to the CCDCamera
-
         try:
             self.camera.LinkEnabled = True
+            self.logger.info("Camera is connected : "+__name__)
         except:
-            self.camera.LinkEnabled==False
+            self.logger.critical("Camera is not connected : "+__name__)
         self.camera.DisableAutoShutdown = True  # All of these settings are just basic camera setup settings.
         self.camera.AutoDownload = True
-
-        #logging portion
-        self.logger = logging.getLogger(__name__ + 'cAmera')     #uses the name and the 'cAmera' logger in the logger.py file    
-        self.logger.info('creating an instance of Camera')       
 
     def expose(self, exposure_time, filter, type="light"):
         if type == "light":
@@ -35,22 +33,19 @@ class Camera:
         self.camera.SetFullFrame()
         self.camera.Expose(exposure_time, type, filter)
         time.sleep(exposure_time)
-        while self.camera.ImageReady==False:
+        while self.camera.ImageReady == False:
             time.sleep(1)
             if self.camera.ImageReady:
-                self.camera.StartDownload
+                # self.camera.StartDownload
                 path = os.path.expanduser('~')
                 self.camera.SaveImage(os.path.join(path, 'Desktop', "test_pictures.fit"))
 
     def log(self):
-        self.logger.info("starting camera module logging")    #a test point so I know if data gets through
-        if self.camera.LinkEnabled:                           #If camera connects, display variable that containd info log
-            a=self.logger.info("Camera has connected")
-            return a                                          #puts out an a for logger.py to collect
-        elif not self.camera.LinkEnabled:                     #If camera does not connect, display variable that contains a critical error
-            a=self.logger.critical("Camera cannot connect")
-            return a
-        self.logger.info("finished camera module logging")
+        self.logger.info("Camera test " + __name__)
+        if self.camera.LinkEnabled:
+            self.logger.info("Camera is connected : "+__name__)
+        elif not self.camera.LinkEnabled:
+            self.logger.critical("Camera cannot connect : "+__name__)
 
     def set_gain(self):
         pass
