a979052a62326ece9e047e4038b6369a297c87bc
Kakon24 <sef1015@rit.edu>
2019-08-06 20:40:09 -0400

    scheduler finished, need to add config reader and driver next

--- main/observing/observation_run.py
+++ main/observing/observation_run.py
@@ -2,9 +2,9 @@ import datetime
 import time
 import os
 
-import main.common.datatype.filter_wheel
-import main.common.util.time_utils as tutil
+from main.common.util import time_utils
 from main.controller.camera import Camera
+from main.common.datatype import filter_wheel
 
 class ObservationRun():
 
@@ -18,65 +18,42 @@ class ObservationRun():
         self.filterwheel_dict = filter_wheel.get_filter().filter_position_dict()
         self.camera = Camera()
 
-    def compare_current_time(self, comparison, future=True):
-        '''
-        Compares current time to comparison.
-
-        If future is true:
-            Make sure comparison is a later datetime than now
-        If future is false:
-            Make sure comparison is an earlier datetime than now
-
-        :param comparison: DateTime obj
-        :param future: Boolean
-        :return: Boolean
-        '''
-        assert isinstance(comparison, datetime.datetime), "Comparison argument must be a datetime object."
-        current_dt = datetime.datetime.now()
-
-        if future and comparison >= current_dt:
-            return True
-
-        if not future and comparison <= current_dt:
-            return True
-
-        return False
-    
-    def take_images(num, exp_time, filter, end_time, path):
-        num_filters = len(filter)
-        
-        for i in range(num):
-            image_name = "SOMETHING" #TODO: need image nomenclature
-            current_filter = filter[num_filters%i]
-            self.camera.expose(int(exp_time), self.filterwheel_dict[current_filter], os.path.join(path, image_name))
-            #TODO: stop observing if end_time is reached.
-
-    
-    def run_ticket(self, ticket, filter=None, cycle=False):
-        if cycle:
-            take_images(ticket.num, ticket.exp_time, ticket.filter, ticket.end_time, path)
-            return
-       
-        if filter:
-            take_images(ticket.num, ticket.exp_time, filter, ticket.end_time, path)
-            return
-        
-        #TODO: if cycle is false?
-            
-
 
     def observe(self):
         
         for ticket in self.observation_request_list:
             #TODO: slew to RA Dec
             #TODO: start guiding
-            if not self.compare_current_time(ticket.start_time, future=False):
-                print("It is not ticket's start time {}, "
-                      "waiting till start time.".format(ticket.start_time.isoformat()))
-                current_epoch_milli = tutil.datetime_to_epoch_milli_converter(datetime.datetime.now)
-                start_time_epoch_milli = tutil.datetime_to_epoch_milli_converter(ticket.start_time)
+            if ticket.start_time > datetime.datetime.now():
+                print("It is not the start time {} of {} observation, "
+                      "waiting till start time.".format(ticket.start_time.isoformat(), ticket.name))
+                current_epoch_milli = time_utils.datetime_to_epoch_milli_converter(datetime.datetime.now)
+                start_time_epoch_milli = time_utils.datetime_to_epoch_milli_converter(ticket.start_time)
                 time.sleep((start_time_epoch_milli - current_epoch_milli)/1000)
                 
-            run_ticket(self, ticket)
-                    
-                
+            self.run_ticket(ticket)
+
+
+    def run_ticket(self, ticket):
+
+        if ticket.cycle_filter:
+            self.take_images(ticket.name, ticket.num, ticket.exp_time,
+                             ticket.filter, ticket.end_time, self.image_directory)
+            return
+
+        for i in range(len(ticket.filter)):
+            self.take_images(ticket.name, ticket.num, ticket.exp_time,
+                             list(ticket.filter[i]), ticket.end_time, self.image_directory)
+
+
+    def take_images(self, name, num, exp_time, filter, end_time, path):
+        num_filters = len(filter)
+
+        for i in range(num):
+            if end_time <= datetime.datetime.now():
+                print("The observations end time of {} has passed.  "
+                      "Stopping observation of {}.".format(end_time, name))
+
+            current_filter = filter[num_filters % i]
+            image_name = "{}_{}_{}_{}.fits".format(name, current_filter, exp_time, datetime.datetime.now().isoformat())
+            self.camera.expose(int(exp_time), self.filterwheel_dict[current_filter], os.path.join(path, image_name))
