6e343c63ce019b4b8633acb33ac4a1535e044304
Nick Pepin <31704762+Warpep19969@users.noreply.github.com>
2019-08-13 00:24:32 -0400

    Update logger.py

--- main/common/logger.py
+++ main/common/logger.py
@@ -1,7 +1,4 @@
 import logging
-from main.common import cAmera
-from main.common.cAmera import *
-
 
 # DEBUG: Detailed info, typically used for diagnosing problems
 # INFO: Confirmation that things are working as expected
@@ -9,31 +6,12 @@ from main.common.cAmera import *
 # ERROR: More serious problem
 # CRITICAL: Very Serious error
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler = logging.FileHandler('C:\\Users\\Nicholas Pepin\\Desktop\\CameraErrorLog.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
-class main:
-    # logging.basicConfig(filename="C:\\Users\\Nicholas Pepin\\Desktop\\CameraErrorLog.log", level=logging.DEBUG,
-    # filemode='w', format="%(asctime)s - %(levelname)s - %(message)s") logger = logging.getLogger()  #These two
-    # lines contain a scrunched version of below but without handlers
-    # creates filehandlers
-    fh = logging.FileHandler('C:\\Users\\Nicholas Pepin\\Desktop\\CameraErrorLog.log')
-    fh.setLevel(logging.DEBUG)
-    # Creates console logger for higher level logging
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
-    # Creates Formatters
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # adds handlers to the logger
-    logger = logging.getLogger()
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-    logger.setLevel(logging.DEBUG)
-
-    logger.info('Camera process has started : ' + __name__)
-    cAmera.Camera.log()  # need to fulfil self
-
-    # Theoretically, it works. but in 'cAmera.Camera.log()' it says that self needs to be fulfilled
-    # and I have no idea how to do that. If someone could help guide me to a paper or video that would
-    # explain it, that would be very helpful.
+logger.info("logger.py works")
     
