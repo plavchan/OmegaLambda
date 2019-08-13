dc8a42f7e960b455479bcd47727a1ccb37d78ad6
Nick Pepin <31704762+Warpep19969@users.noreply.github.com>
2019-08-13 00:24:58 -0400

    Update test_driver.py

--- main/drivers/test_driver.py
+++ main/drivers/test_driver.py
@@ -1,10 +1,6 @@
 from main.common.cAmera import *
-from main.common.logger import *
 
-#activates the camera function
+# activates the camera function
 camera_object = Camera("camera_work")
 camera_object.expose(10, 1, type="dark")
-
-#activates the logger function
-main('camera_work')
-#NEEDS TESTING
+# NEEDS TESTING
