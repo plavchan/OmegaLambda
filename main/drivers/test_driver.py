e9ceee976af3c7fea56e487fd6bbc3b2693eff07
Michael <michael.reefe8@gmail.com>
2020-02-17 16:20:02 -0500

    Changes made during debugging.  Currently works except for the cooler and image save path.

--- main/drivers/test_driver.py
+++ main/drivers/test_driver.py
@@ -1,6 +1,9 @@
 from main.controller.camera import Camera
 
 camera_object = Camera()
-camera_object.expose(10, 1, type="dark")
+#camera_object.check_connection()
+#camera_object.coolerSet()
+
+camera_object.expose(10, 1, r'c\users\gmuobservatory\documents\observing sessions\2020_data', type="dark")
 
 #NEEDS TESTING
