e83a12583e9d2aca3f087d829b421e206c38c5b1
Michael <michael.reefe8@gmail.com>
2020-02-17 16:13:25 -0500

    Edits based on testing with dev.  See testing log.txt for details.

--- main/controller/camera.py
+++ main/controller/camera.py
@@ -9,8 +9,7 @@ class Camera():
     def __init__(self):
         self.Camera = win32com.client.Dispatch("MaxIm.CCDCamera")  # Sets the camera connection path to the CCDCamera
         self.Camera.DisableAutoShutdown = True  # All of these settings are just basic camera setup settings.
-        self.Camera.LockApp = True
-        self.Camera.CoolerOn = True
+        #self.Camera.LockApp = True
         self.Camera.AutoDownload = True
 
         self.check_connection()
@@ -19,13 +18,25 @@ class Camera():
 
     def check_connection(self):
         if self.Camera.LinkEnabled == True:
-            print("Camera is connected")
+            print("Camera is already connected")
         else:
-            print("Camera is not connected")
+            try:
+                self.Camera.LinkEnabled = True
+            except:
+                print("Camera cannot connect")
+            else:
+                print("Camera has successfully connected")
     
     def coolerSet(self):
         global T_0
-        self.Camera.SetCCDTemperature(T_0) #Can use as a method to set the CCD temp., or as a property to call the current setpoint
+        if self.Camera.CoolerOn == True and self.Camera.SetCCDTemperature == T_0:
+            pass
+        else:
+            try:
+                self.Camera.CoolerOn = True
+                self.Camera.SetCCDTemperature(T_0) #Can use as a method to set the CCD temp., or as a property to call the current setpoint
+            except:
+                return print("Cooler Error")
     
     def coolerAdjust(self): #Heavy wip to find the right way to adjust cooler temp. based on cooler power
         global T_0
@@ -36,7 +47,7 @@ class Camera():
             self.Camera.SetCCDTemperature(T_0 + 5)
 
     def expose(self, exposure_time, filter, save_path, type="light"):
-        if type == "light":
+        if type == "light": #probably will have to change this, 1 translated to the UV filter
             type = 1
         elif type == "dark":
             type = 0
@@ -45,12 +56,12 @@ class Camera():
             return
         self.Camera.SetFullFrame()
         self.Camera.Expose(exposure_time, type, filter) #In the pdf it says the method is called "StartExposure(duration,type)"??
-        while Camera.ImageReady==False:
+        while self.Camera.ImageReady==False:
             time.sleep(1)
-            if Camera.ImageReady:
-                Camera.StartDownload
+            if self.Camera.ImageReady:
+                self.Camera.StartDownload
                 #TODO: Automate image nomenclature 
-                Camera.SaveImage(save_path)
+                self.Camera.SaveImage(save_path)
 
     def set_gain(self):
         self.Camera.SetupDialog #Opens dialog box for user to input settings?  Couldn't find a gain method in the resources pdf.
