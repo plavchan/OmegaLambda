b774d60de8ff0b2508c0257d1d7c4241c0f10446
Michael <michael.reefe8@gmail.com>
2020-03-30 14:37:01 -0400

    New edits based on testing.  Was able to get a successful test observation run where the camera took several images in different filters continuously (based on the test.json in resources).

--- main/drivers/run_test_driver.py
+++ main/drivers/run_test_driver.py
@@ -8,8 +8,15 @@ import os
 folder = (datetime.datetime.now(datetime.timezone.utc)).strftime('%Y%m%d')
 try: os.mkdir(r'h:\observatory files\observing sessions\2020_data\{0:s}'.format(folder))
 except: print('ERROR: Could not create directory')
+else: print('New directory for tonight\'s observing has been made!')
 
-json_reader = Reader(r'c:\users\gmu observtory1\-omegalambda\resources\test.json')
-object_reader = ObjectReader(json_reader)
+try: json_reader = Reader(r'c:\users\gmu observtory1\-omegalambda\resources\test.json')
+except: print('ERROR: Error reading observation ticket')
+
+try: object_reader = ObjectReader(json_reader)
+except: print('ERROR: Error reading observation ticket')
+else: print('Observation ticket has been read.  Starting observation run!')
 
 run_object = ObservationRun([object_reader.ticket], r'h:\observatory files\observing sessions\2020_data\{0:s}'.format(folder))
+run_object.observe()
+
--- main/observing/observation_run.py
+++ main/observing/observation_run.py
@@ -4,8 +4,9 @@ import os
 
 from main.common.util import time_utils
 from main.controller.camera import Camera
-from main.common.datatype import filter_wheel
-from main.controller.telescope import Telescope
+from main.common.IO.json_reader import Reader
+from main.common.datatype.object_reader import ObjectReader
+#from main.controller.telescope import Telescope
 
 class ObservationRun():
 
@@ -16,20 +17,25 @@ class ObservationRun():
         '''
         self.image_directory = image_directory
         self.observation_request_list = observation_request_list
-        self.filterwheel_dict = filter_wheel.get_filter().filter_position_dict()
         self.camera = Camera()
-        self.telescope = Telescope()
+        #self.telescope = Telescope()
+        
+        self.filterwheel_dict = {value: int(key.replace('position_', '')) for key,value in
+                                 ObjectReader(Reader(r'c:\users\gmu observtory1\-omegalambda\config\fw_config.json')).ticket.__dict__.items()}
+        #Let me know if this is what you were thinking for the filterwheel--since I had to reverse keys and values to get an int for each filter to pass to camera.expose()
+        
 
     def observe(self):
         
         for ticket in self.observation_request_list:
             if ticket.ra:
-                self.telescope.telescopemove(ticket.ra, ticket.dec)
-                self.run_ticket(ticket)
-                return
+                #self.telescope.telescopemove(ticket.ra, ticket.dec)
+                #self.run_ticket(ticket)
+                pass
         
             for i in range(len(ticket.ra)):
-                self.telescope.telescopemove(ticket.ra[i], ticket.dec[i])
+                #self.telescope.telescopemove(ticket.ra[i], ticket.dec[i])
+                pass
             
             
             #TODO: start guiding
@@ -51,19 +57,17 @@ class ObservationRun():
 
         for i in range(len(ticket.filter)):
             self.take_images(ticket.name, ticket.num, ticket.exp_time,
-                             list(ticket.filter[i]), ticket.end_time, self.image_directory)
+                             list(ticket.filter)[i], ticket.end_time, self.image_directory)
 
 
-    def take_images(self, name, num, exp_time, filter, end_time, path):
-        num_filters = len(filter)
-        
+    def take_images(self, name, num, exp_time, current_filter, end_time, path):
+    
         image_num = 1
         for i in range(num):
             if end_time <= datetime.datetime.now():
                 print("The observations end time of {} has passed.  "
                       "Stopping observation of {}.".format(end_time, name))
-
-            current_filter = filter[num_filters % i]
+            
             image_name = "{0:s}_{1:d}s_{2:s}-{3:04d}.fits".format(name, exp_time, current_filter, image_num)
             self.camera.expose(int(exp_time), self.filterwheel_dict[current_filter], os.path.join(path, image_name), type="light")
             image_num += 1
