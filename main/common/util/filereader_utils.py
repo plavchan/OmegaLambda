bc3066f02dd6920c5fb31c2df24f2e85496c7f4a
oalfaro2 <owenalfaro@gmail.com>
2020-07-02 13:24:38 -0400

    Added function to get FWHMs from fits file FWHMs returned are in whole numbers

--- main/common/util/filereader_utils.py
+++ main/common/util/filereader_utils.py
@@ -1,10 +1,12 @@
 # Filereader Utils for Focuser & Guider
 import logging
 import statistics
+import numpy as np
 
 import photutils
 from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
+# import matplotlib.pyplot as plt
 
 def IRAF_calculations(path, **kwargs):
     '''
@@ -51,3 +53,76 @@ def IRAF_calculations(path, **kwargs):
     if not results:
         results = fitsfile
     return results
+
+def GaussianFit(x, a, x0, sigma):
+    return a*exp(-(x-x0)**2/(2*sigma**2))
+
+def Radial_Average(path):
+
+    image = fits.getdata(path)
+    mean, median, stdev = sigma_clipped_stats(image, sigma = 3)
+    threshold = photutils.detect_threshold(image, nsigma = 15)
+    starfound = photutils.find_peaks(image, threshold = threshold)
+    R = 12
+    r = list(range(0,4096))
+    data = image - median
+    pix_x, pix_y = np.indices((data.shape))
+    n = 0
+    fwhm_list = []
+
+    for row in starfound:
+    
+        x_cent = starfound['x_peak'][n]
+        y_cent = starfound['y_peak'][n]
+        n = n+1
+        xmin = int(x_cent - R)
+        xmax = int(x_cent + R)
+        ymin = int(y_cent - R)
+        ymax = int(y_cent + R)
+        star = data[ymin:ymax, xmin:xmax]
+        starx, stary = np.indices((star.shape))
+        r = np.sqrt((stary - R)**2 + (starx - R)**2)
+        r = r.astype(np.int)
+
+        tbin = np.bincount(r.ravel(), star.ravel())
+        nr = np.bincount(r.ravel())
+        radialprofile = tbin / nr
+
+       
+        if len(radialprofile) != 0:
+            maximum = radialprofile[0]
+            if radialprofile[2] > (10 +stdev):
+                num = int(0)
+                run = True
+                for x in radialprofile:
+                    if run == True:
+                        if radialprofile[num] <= (maximum/2):
+                            #print('HWHM = {}'.format(num))
+                            FWHM = num*2
+                            fwhm_list = np.append(fwhm_list, FWHM)
+                            run = False
+
+                        num = num + 1
+                    elif run == False:
+                        break
+                
+                # graphing stuff 
+                
+                # plt.figure()
+                # plt.imshow(star, cmap = 'YlGn')
+                # if z == 4:
+                #       f = list(range(0,len(radialprofile)))
+                #     # fig,ax=plt.subplots()
+                #     # ax.plot(f,radialprofile)
+                #     plt.plot(f, radialprofile, 'b+:', label='data')
+                #     plt.plot(f, GaussianFit(f, *popt), 'ro:', label='fit')
+                #     plt.legend()
+                #     plt.xlabel('x position')
+                #     plt.ylabel('flux')
+                #     plt.show()
+                #     plt.savefig(r'C:/Users/GMU Observtory1/-omegalambda/test/plot.png')
+                
+        mask = [fwhm >= 4 for fwhm in fwhm_list]
+
+
+    return fwhm_list[mask]
