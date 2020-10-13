a55d07c4d505ff2754d6239b563c3c85e4e024ce
Owen Alfaro <owenalfaro@Owens-MBP.home>
2020-10-13 15:43:31 -0400

    Created new file thread_monitor to run side by side with main code

---
+++ main/controller/thread_monitor.py
@@ -0,0 +1,11 @@
+import os
+import threading
+
+
+class monitor(threading.Thread):
+    def __init__(self):
+        self.label
+        self.threadlist = threading.enumerate()
+
+    def monitor_run(self):
+
