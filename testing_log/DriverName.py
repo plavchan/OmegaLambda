dc7df2fa92118007d974821cedbaf9eb55735a4e
oalfaro2 <67032452+oalfaro2@users.noreply.github.com>
2020-10-23 10:57:53 -0400

    Delete DriverName.py

--- testing_log/DriverName.py
+++
@@ -1,8 +0,0 @@
-import win32com.client
-
-x = win32com.client.Dispatch("ASCOM.Utilities.Chooser")
-x.DeviceType = "Camera"
-driver = x.Choose(None)
-print("The driver is" + driver)
-
-win32com.client.Dispatch(driver)
