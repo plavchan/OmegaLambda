9f370f56edb5ea357e3c790bd6966cc8887aec8b
Michael Reefe <57511178+Michael-Reefe@users.noreply.github.com>
2021-01-21 21:14:58 -0500

    added telescope coordinate check for hour angles > 9h

--- main/controller/telescope.py
+++ main/controller/telescope.py
@@ -6,6 +6,7 @@ import pywintypes
 import win32com.client
 
 from ..common.util import conversion_utils
+from ..common.util import time_utils
 from .hardware import Hardware
 
 
@@ -87,11 +88,15 @@ class Telescope(Hardware):
             slew may proceed.
 
         """
+        lst = time_utils.get_local_sidereal_time(self.config_dict.site_longitude, time)
+        ha = lst - ra # in hours
         (az, alt) = conversion_utils.convert_radec_to_altaz(ra, dec, self.config_dict.site_latitude,
                                                             self.config_dict.site_longitude, time)
         logging.debug('Checking coordinates for telescope slew...')
-        if alt <= 15 or dec > 90:
-            logging.debug('Coordinates not good.  Aborting slew.')
+        if (alt <= 15) or (dec > 90) or (abs(ha) > 8.75):
+            msg = "Altitude less than 15 degrees" if (alt <= 15) else "Declination above 90 degrees" if (dec > 90) else \
+                "Hour angle greater than 8h 45m" if (abs(ha) > 8.75) else "None"
+            logging.error('Coordinates not good.  Aborting slew.  Reason: {}'.format(msg))
             return False
         else:
             logging.debug('Coordinates are good.  Starting slew')
