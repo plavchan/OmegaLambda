ee60b4c884a657f386cfa48744226de779282e3b
Michael Reefe <57511178+Michael-Reefe@users.noreply.github.com>
2021-05-20 11:52:06 -0400

    small sleep time adjustment

--- omegalambda/main/observing/observation_run.py
+++ omegalambda/main/observing/observation_run.py
@@ -306,7 +306,9 @@ class ObservationRun:
             current_time = datetime.datetime.now(self.tz)
             current_epoch_milli = time_utils.datetime_to_epoch_milli_converter(current_time)
             start_time_epoch_milli = time_utils.datetime_to_epoch_milli_converter(ticket.start_time)
-            time.sleep((start_time_epoch_milli - current_epoch_milli) / 1000)
+            dt = (start_time_epoch_milli - current_epoch_milli) / 1000
+            if dt > 0:
+                time.sleep(dt)
         return shutdown, cooler
 
     def observe(self):
