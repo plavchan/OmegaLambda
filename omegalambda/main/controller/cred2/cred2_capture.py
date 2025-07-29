d3aee3310f9fb84c7915019c070fe712c53dd5e0
Alan Zhu <90356452+alanzhu0@users.noreply.github.com>
2025-07-29 02:19:52 -0400

    last ndr fix

--- omegalambda/main/controller/cred2/cred2_capture.py
+++ omegalambda/main/controller/cred2/cred2_capture.py
@@ -848,21 +848,18 @@ def uptheramp_thread() -> None:
         total_ndrs += 1
         ndr_num = image[0][2]  # The third pixel in the image holds the current NDR number
         if last_ndr_num < ndr_num < NDR_NUM:
-            if first_image or skip_next_resultant or len(images) < 0.7 * NDR_NUM:
-                if first_image:  # discard the first image because it probably doesn't have all the NDRs
-                    first_image = False
-                if skip_next_resultant:
-                    skip_next_resultant = False
-                if len(images) < 0.7 * NDR_NUM:
-                    print(f"Warning: received NDR number {ndr_num} but only {len(images)} images in the queue. This may indicate a problem with taking NDRs. Skipping for now...")
-                    last_ndr_num = ndr_num
-                images.clear()
-                uptheramp_queue.task_done()
-                continue
-            resultant = uptheramp_fit(images)
-            if resultant is not None:
-                resultants.append(resultant)
-                ndr_groups += 1
+            if first_image:  # discard the first image because it probably doesn't have all the NDRs
+                first_image = False
+            elif skip_next_resultant:
+                skip_next_resultant = False
+            elif len(images) < 0.7 * NDR_NUM:
+                print(f"Warning: received NDR number {ndr_num} but only {len(images)} images in the queue. This may indicate a problem with taking NDRs. Skipping for now...")
+                last_ndr_num = ndr_num
+            else:
+                resultant = uptheramp_fit(images)
+                if resultant is not None:
+                    resultants.append(resultant)
+                    ndr_groups += 1
             images.clear()
         
         images.append(image)
