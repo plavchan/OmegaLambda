f3842fdb096f2c103674c3ca71017faaa1f32c79
Peter Plavchan <plavchan@gmail.com>
2026-06-19 21:54:39 -0400

    dome.py 20260619

--- omegalambda/main/controller/dome.py
+++ omegalambda/main/controller/dome.py
@@ -26,6 +26,7 @@ class Dome(Hardware):
         self.has_homed = threading.Event()
         self.dome_move_lock = threading.Lock()
         self.shutter = None
+        self.domedone = True
         super(Dome, self).__init__(name='Dome')
 
     def check_connection(self):
